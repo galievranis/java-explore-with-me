44e49ca9906d5ce19f144362ddf9e3d500bfd9a3
galievranis <galiev.ranisque@gmail.com>
2023-06-03 11:22:47 +0600

    fix: удалить аннотацию @Repository с репозитория статистики

--- stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -2,14 +2,12 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.model.EndpointHit;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query("SELECT new ru.practicum.dto.ViewStatsDto(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
